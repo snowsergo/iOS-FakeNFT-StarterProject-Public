9e9ea08092c8529b4a47040677cadd020d234c70
developer <mahlenko-weblive@yandex.ru>
2023-05-19 00:23:54 +0300

    Base markup screen

--- FakeNFT/Сart/CartViewController.swift
+++ FakeNFT/Сart/CartViewController.swift
@@ -7,9 +7,91 @@ import UIKit
 /**
  General screen controller for Cart Screen
  */
-class CartViewController: UIViewController {
+final class CartViewController: UIViewController {
+
+    let filterButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Фильтр", for: .normal)
+        button.backgroundColor = .systemGray3
+        return button
+    }()
+
+    let collectionView: UIView = {
+        let collectionView = UIView()
+        collectionView.backgroundColor = .systemGray2
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
+    let footerView: UIView = {
+        let footerView = UIView()
+        footerView.backgroundColor = .systemGray
+        footerView.translatesAutoresizingMaskIntoConstraints = false
+        return footerView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.subviews.forEach({ $0.removeFromSuperview() })
+
+        view.backgroundColor = .systemGray6
+
+        view.addSubview(filterButton)
+        view.addSubview(collectionView)
+        view.addSubview(footerView)
+
+        filterButton.addTarget(self, action: #selector(sortByTouch), for: .touchUpInside)
+
+        setupConstraints()
+    }
+
+    // MARK: - @IBActions
+
+    @objc func sortByTouch(sender: Any) {
+        let alertController = UIAlertController(title: "Сортировка", message: nil, preferredStyle: .actionSheet)
+
+        let actions: [UIAlertAction] = [
+            UIAlertAction(title: "По цене", style: .default) { action in
+                print("Выбрана сортировка: \(action.title!)")
+            },
+            UIAlertAction(title: "По рейтингу", style: .default) { action in
+                print("Выбрана сортировка: \(action.title!)")
+            },
+            UIAlertAction(title: "По названию", style: .default) { action in
+                print("Выбрана сортировка: \(action.title!)")
+            },
+            UIAlertAction(title: "Закрыть", style: .cancel)
+        ]
+
+        actions.forEach { action in
+            alertController.addAction(action)
+        }
+
+        present(alertController, animated: true)
+    }
+
+    // MARK: - Private methods
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            // constraints for filter button
+            filterButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            filterButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            filterButton.widthAnchor.constraint(equalToConstant: 42),
+            filterButton.heightAnchor.constraint(equalToConstant: 42),
+
+            // constraints for collection view
+            collectionView.topAnchor.constraint(equalTo: filterButton.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: footerView.topAnchor),
+
+            // constraints for footer
+            footerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            footerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            footerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            footerView.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
 }
