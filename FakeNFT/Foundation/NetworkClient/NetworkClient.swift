74e567174704bbf040fa24e167e253a734f5c944
developer <mahlenko-weblive@yandex.ru>
2023-05-27 01:16:04 +0300

    Add httpBody and application/json header.

--- FakeNFT/Foundation/NetworkClient/NetworkClient.swift
+++ FakeNFT/Foundation/NetworkClient/NetworkClient.swift
@@ -84,7 +84,9 @@ struct DefaultNetworkClient: NetworkClient {
         guard let url = components.url else { return nil }
 
         var urlRequest = URLRequest(url: url)
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
         urlRequest.httpMethod = request.httpMethod.rawValue
+        urlRequest.httpBody = request.httpBody
 
         return urlRequest
     }
