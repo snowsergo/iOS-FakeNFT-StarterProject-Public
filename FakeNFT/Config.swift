7a8ad907834959ea38cad4d2cf0409f8b6b3702b
developer <mahlenko-weblive@yandex.ru>
2023-05-27 23:58:06 +0300

    Use asset icons and update request

---
+++ FakeNFT/Config.swift
@@ -0,0 +1,10 @@
+//
+// Created by Сергей Махленко on 27.05.2023.
+//
+
+import Foundation
+
+struct Config {
+    static let mockOrderId = "1"
+    static let baseUrl = "https://64611c69491f9402f49ecce1.mockapi.io"
+}
