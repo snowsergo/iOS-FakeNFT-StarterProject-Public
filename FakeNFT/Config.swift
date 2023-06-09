fc2f230a53190c58e7d4bba9f48e4b3d02b26fcf
developer <mahlenko-weblive@yandex.ru>
2023-06-10 01:13:45 +0300

    Use first order

--- FakeNFT/Config.swift
+++ FakeNFT/Config.swift
@@ -7,7 +7,7 @@
 import Foundation
 
 struct Config {
-    static let mockOrderId = "2"
+    static let mockOrderId = "1"
     static let baseUrl = "https://64611c69491f9402f49ecce1.mockapi.io/api/v1"
     static let userAgreementUrl = "https://yandex.ru/legal/practicum_termsofuse"
 }
