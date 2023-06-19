b781ccc82896795eaeb729185a53a1f8137bccdf
developer <mahlenko-weblive@yandex.ru>
2023-06-19 22:15:02 +0300

    Update user default key

--- FakeNFT/Config.swift
+++ FakeNFT/Config.swift
@@ -11,7 +11,7 @@ struct Config {
     static let baseUrl = "https://64611c69491f9402f49ecce1.mockapi.io/api/v1"
     static let userAgreementUrl = "https://yandex.ru/legal/practicum_termsofuse"
 
-    static let cartSortKey = "cartSortKey"
+    static let cartSortKey = "cart:sortKey"
 }
 
 // swiftlint:enable convenience_type
